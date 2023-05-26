87621538a085a9355e93e48b99fa6a6845571dcb
LeuIon <ion.leu.2017@mail.ru>
2023-05-26 11:23:22 +0300

    Laboratorul_2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Leu Ion CR-202</h1>
+    </div>
+  );
+}
+
+export default App;
