74ab240a32454d1bfa008f929452083f4772d4c2
LeuIon <ion.leu.2017@mail.ru>
2023-05-29 17:15:45 +0300

    Laboratorul_3

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Content from './components/Content'
+import FormCar from "./components/FormCar";
+import Footer from "./components/Footer";
+import {data, ICar} from "./data";
+
+function App() {
+    const[cars,setCars] = useState(data)
+    const createNewitem = (item : ICar)=>{
+        setCars(prevState => [...prevState,item])
+        console.log(cars)
+    }
+    return (
+        <div>
+            <Header />
+            <Content data={cars}/>
+            <FormCar createCar={(item:ICar)=>createNewitem(item)} />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
